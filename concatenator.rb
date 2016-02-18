e75f7e87d345361073fee9267b7c06318914dbe6
Cory Spitzer <coryspitzer@gmail.com>
2016-02-18 10:08:01 -0500

    Concatenate arrays

--- concatenator.rb
+++ concatenator.rb
@@ -1,3 +1,8 @@
 def concatenate(array0, array1)
-
+  # array0 + array1 will work,
+  # but how can we write that functionality ourselves?
+  array1.each do |element|
+    array0 << element
+  end
+  array0
 end
--- concatenator_spec.rb
+++ concatenator_spec.rb
@@ -5,4 +5,8 @@ describe '#concatenate' do
   it 'handles two empty arrays' do
     expect(concatenate([], [])).to eq []
   end
+
+  it 'handles arrays with multiple elements' do
+    expect(concatenate([2, 3], [1, 'l'])).to eq [2, 3, 1, 'l']
+  end
 end
